71e554e3d1a5784b3f3b5ca86107c8cae3adb325
CarlosArango <arangotorcar@gmail.com>
2024-11-14 18:40:16 -0500

    feat: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { UserPreferencesProvider } from "@/providers/UserPreferencesProvider";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import MainLayout from "@/components/MainLayout";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning lang="es">
+      <GoogleTagManager gtmId="GTM-MV333992" />
       <body className={`${poppins.className} antialiased`}>
         <UserPreferencesProvider>
           <MainLayout>{children}</MainLayout>
