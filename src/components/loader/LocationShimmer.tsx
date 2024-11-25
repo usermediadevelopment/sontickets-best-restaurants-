c8267c3fb8b99f35439e8a3cd7f1b4f5b4a547fd
Carlos <arangotorcar@gmail.com>
2024-11-25 17:00:49 -0500

    location shimmer

--- src/components/loader/LocationShimmer.tsx
+++ src/components/loader/LocationShimmer.tsx
@@ -37,15 +37,21 @@ export const ImageSwiperShimmer = () => {
 
 export const ContentShimmer = () => {
   return (
-    <div className="flex flex-row mt-10 p-4">
+    <div className="flex  flex-col md:flex-row md:mt-10 p-4 gap-4">
       <div className="flex flex-col flex-grow gap-2">
         <div className="w-[70px] h-3 bg-gray-200 rounded-md" />
         {/* Título de carga */}
-        <div className="w-7/12 h-4 bg-gray-200 rounded-md" />
-        <div className="w-3/12 h-4 bg-gray-200 rounded-md" />
-        <div className="w-2/12 h-4 bg-gray-200 rounded-md" />
+        <div className="md:w-2/12 h-4 bg-gray-200 rounded-md" />
+        <div className="md:w-6/12 h-4 bg-gray-200 rounded-md" />
+        <div className="md:w-4/12 h-4 bg-gray-200 rounded-md" />
 
-        <div className="w-7/12 h-4 bg-gray-200 rounded-md" />
+        <div className="md:w-7/12 h-4 bg-gray-200 rounded-md" />
+
+        <div className="md:w-10/12 h-4 bg-gray-200 rounded-md mt-4" />
+        <div className="md:w-6/12 h-4 bg-gray-200 rounded-md" />
+        <div className="md:w-4/12 h-4 bg-gray-200 rounded-md" />
+
+        <div className="md:w-7/12 h-4 bg-gray-200 rounded-md" />
       </div>
       <div className="flex">
         <div className="w-96  h-72 bg-gray-200 rounded-md" />
