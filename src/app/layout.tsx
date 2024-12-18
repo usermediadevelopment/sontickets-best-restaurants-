4d7ee6acf9dcf1297e43364d06e8e6a84c389d27
Carlos <arangotorcar@gmail.com>
2024-12-18 17:34:56 -0500

    feat: add canonical

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,35 @@ import "./globals.css";
 import { Poppins } from "next/font/google";
 import MainLayout from "@/components/MainLayout";
 import { GoogleTagManager } from "@next/third-parties/google";
+import { Metadata } from "next";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   display: "swap",
 });
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Mejores Restaurantes",
+    description:
+      "Explora los mejores restaurantes de Colombia. Encuentra experiencias gastronómicas únicas y recomendaciones en tu ciudad favorita",
+
+    alternates: {
+      canonical: "https://mejoresrestaurantes.co/",
+    },
+    openGraph: {
+      images: [
+        {
+          url: "https://mejoresrestaurantes.co/_next/image?url=%2Flogo.png&w=384&q=75",
+          width: 800,
+          height: 600,
+          alt: "Mejores Restaurantes",
+        },
+      ],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
